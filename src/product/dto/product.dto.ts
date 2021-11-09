57a7c52871cd09592abe476bc69e9a49507210ba
Aaron Carrasco <vaaroncarrasco@gmail.com>
2021-11-09 04:05:29 -0500

    product schema and dto modified

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,7 +1,10 @@
 export class CreateProductDTO {
   readonly name: string;
+  readonly owner: string;
+  readonly category: string;
   readonly description: string;
   readonly imageURL: string;
   readonly price: number;
+  readonly stock: number;
   readonly createdAt: Date;
 }
--- src/product/schema/product.schema.ts
+++ src/product/schema/product.schema.ts
@@ -1,12 +1,42 @@
-import { Schema } from 'mongoose';
+import { Document, Schema } from 'mongoose';
+import { Product } from '../interfaces/product.interface';
 
-export const ProductSchema = new Schema({
-  name: { type: String, required: true },
-  description: String,
-  imageURL: String,
-  price: Number,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+export type ProductDocument = Product & Document;
+
+// export const ProductSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: [true, 'product must have a name'],
+//     trim: true,
+//     maxlength: [40, 'product name max length is 40 characters'],
+//     minlength: [10, 'product name min length is 10 characters'],
+//   },
+//   category: {
+//     type: String,
+//     required: [true, 'product must have a category'],
+//     trim: true,
+//   },
+//   description: {
+//     type: String,
+//     required: [true, 'product must have a description'],
+//     trim: true,
+//   },
+//   imageURL: {
+//     type: String,
+//     required: [true, 'product must have an image'],
+//     trim: true,
+//   },
+//   price: {
+//     type: Number,
+//     required: [true, 'product must have a price'],
+//   },
+//   stock: {
+//     type: Number,
+//     requried: [true, 'stock must be either 0 or more'],
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now(),
+//     select: false,
+//   },
+// });
