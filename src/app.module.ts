007c345410ef5c368d35601fda988853b961eb17
Aaron Carrasco <vaaroncarrasco@gmail.com>
2021-11-09 05:15:03 -0500

    user controller module and service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,16 @@ import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/test-my-stock'),
     ProductModule,
+    UserModule,
   ],
-  controllers: [AppController, ProductController],
+  controllers: [AppController, ProductController, UserController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
