1e8bbd9a092270d1dd4edac706ed54263c35a3a6
Aaron Carrasco <vaaroncarrasco@gmail.com>
2021-11-09 03:57:03 -0500

    interface modified

--- src/product/interfaces/product.interface.ts
+++ src/product/interfaces/product.interface.ts
@@ -2,8 +2,11 @@ import { Document } from 'mongoose';
 
 export interface Product extends Document {
   readonly name: string;
+  readonly owner: string;
+  readonly category: string;
   readonly description: string;
   readonly imageURL: string;
   readonly price: number;
+  readonly stock: number;
   readonly createdAt: Date;
 }
